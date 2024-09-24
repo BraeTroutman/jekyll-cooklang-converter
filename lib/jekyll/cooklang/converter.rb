f394c3f3a43576fa25868cbecad765e9aa9ac418
BraeTroutman <btroutma@redhat.com>
2024-09-24 13:54:57 -0400

    initial commit

---
+++ lib/jekyll/cooklang/converter.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "converter/version"
+
+module Jekyll
+  module Cooklang
+    module Converter
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/jekyll/cooklang/converter/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Jekyll
+  module Cooklang
+    module Converter
+      VERSION = "0.1.0"
+    end
+  end
+end
